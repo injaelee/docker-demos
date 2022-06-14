3d1b1c1ab4c0b1a825fa2cf1ff03cb51f18f1e8f
injaelee <injaelee@gmail.com>
2022-06-13 20:48:15 -0700

    cli: added better order book display

--- fluent/trading-cli.py
+++ fluent/trading-cli.py
@@ -74,7 +74,7 @@ class TokenAmountBuilder:
     ) -> Union[str,IssuedCurrencyAmount]:
 
         if token_name == "XRP":
-            return str(int(token_amount) * 1_000_000)
+            return str(int(float(token_amount) * 1_000_000))
 
         issuer = cls.TOKEN_ISSUER_MAPPING.get(token_name)
         return IssuedCurrencyAmount(
@@ -305,18 +305,19 @@ class DEXShell(cmd.Cmd):
         )
         print(response)
 
-    def do_trade_order(self,
+    def do_trade(self,
         arg: str,
     ):
         """ trader_order [wallet_name] [order] [qtyA] [tokenA] @ [qtyB] [tokenB] (per token price)"""
-        """ 
+        """
             Examples:
             trader_order walletName sell 100 IJL @ 12 XRP --> Taker Gets 1200 XRP ; Pays  100 IJL
             trader_order walletName buy  100 IJL @ 12 XRP --> Taker Gets  100 IJL ; Pays 1200 XRP
         """
+        cmd_name = "trade"
         arg_values = [entry for entry in arg.split()]
         if len(arg_values) != 7:
-            print(f"[trade_order] Missing arguments. Got {arg_values}")
+            print(f"[{cmd_name}] Missing arguments. Got {arg_values}")
             return
 
         wallet_name, order_type, qty_A, token_A, _, price_B_per_A, token_B = arg_values
@@ -338,16 +339,15 @@ class DEXShell(cmd.Cmd):
             taker_pays_token = token_A
             taker_pays_qty = int(qty_A)
         else:
-            print(f"[trader_order] Order type '{order_type}' does not exist.")
+            print(f"[{cmd_name}] Order type '{order_type}' does not exist.")
             return
 
-        print(f"Taker Gets: [{taker_gets_token}] {taker_gets_qty}")
-        print(f"Taker Pays: [{taker_pays_token}] {taker_pays_qty}")
+        print(f"[{cmd_name}] Taker Gets: [{taker_gets_token}] {taker_gets_qty}")
+        print(f"[{cmd_name}] Taker Pays: [{taker_pays_token}] {taker_pays_qty}")
 
         account_wallet = self.named_wallet.get(wallet_name)
-        import pdb; pdb.set_trace()
         if not account_wallet:
-            print(f"[trade_order] Account for wallet '{wallet_name}' does not exist.")
+            print(f"[{cmd_name}] Account for wallet '{wallet_name}' does not exist.")
             return
 
         offer_create_tx = OfferCreate(
@@ -358,26 +358,36 @@ class DEXShell(cmd.Cmd):
             ),
             taker_pays = TokenAmountBuilder.build_amount(
                 token_name = taker_pays_token,
-                token_amount = taker_gets_qty,
+                token_amount = taker_pays_qty,
             ),
         )
+
+        print(offer_create_tx)
+
         offer_prepared = xrpl.transaction.safe_sign_and_autofill_transaction(
             transaction = offer_create_tx,
             wallet = account_wallet,
             client = self.xrpl_client,
         )
-        response = xrpl.transaction.send_reliable_submission(
-            offer_prepared,
-            self.xrpl_client,
-        )
-        print(response)        
+
+        try:
+            response = xrpl.transaction.send_reliable_submission(
+                offer_prepared,
+                self.xrpl_client,
+            )
+        except Exception as exp:
+            print("[{cmd_name}] transaction failed.")
+            print(exp)
+            return
+
+        print(response)
 
 
     def do_offer_create(self,
         arg: str,
     ):
         """offer_create [wallet_name] [sell_quantity]:[sell_token_name] [buy_quantity]:[buy_token_name]"""
-        
+
         arg_values = [entry for entry in arg.split()]
 
         if len(arg_values) != 3:
@@ -417,6 +427,8 @@ class DEXShell(cmd.Cmd):
         arg: str,
     ):
         pair_tokens = arg.split(":")
+
+        # book offers of the asks
         book_offers_req = BookOffers(
             taker_gets = TokenAmountBuilder.build_token(pair_tokens[0]),
             taker_pays = TokenAmountBuilder.build_token(pair_tokens[1]),
@@ -424,20 +436,41 @@ class DEXShell(cmd.Cmd):
         resp = self.xrpl_client.request(book_offers_req)
 
         if not resp.is_successful():
-            print(f"[order_book] error calling the order book for '{arg}' pair.")
+            print(f"[order_book] error calling asks side of the order book for '{arg}' pair.")
             return
 
         def __sep(order):
             if type(order) == dict:
-                return order.get("currency"), int(order.get("value"))
+                return order.get("currency"), float(order.get("value"))
             return "XRP", int(order) / 1_000_000.
 
+        print("# Asks / Offers")
         for offer in resp.result.get("offers"):
             gets_token, gets_value = __sep(offer.get("TakerGets"))
             pays_token, pays_value = __sep(offer.get("TakerPays"))
             quality = offer.get("quality")
             price = pays_value / gets_value
             print(f"{gets_value} {gets_token} @ {price} {pays_token}")
+
+        # book offers of the bids
+        book_offers_req = BookOffers(
+            taker_gets = TokenAmountBuilder.build_token(pair_tokens[1]),
+            taker_pays = TokenAmountBuilder.build_token(pair_tokens[0]),
+        )
+        resp = self.xrpl_client.request(book_offers_req)
+
+        if not resp.is_successful():
+            print(f"[order_book] error calling bids side of the order book for '{arg}' pair.")
+            return
+
+        print("\n# Bids")
+        for offer in resp.result.get("offers"):
+            gets_token, gets_value = __sep(offer.get("TakerGets"))
+            pays_token, pays_value = __sep(offer.get("TakerPays"))
+            quality = offer.get("quality")
+            price =  gets_value / pays_value
+            print(f"{pays_value} {pays_token} @ {price} {gets_token}")
+
 
     def do_quit(self,
         arg: str,
@@ -453,7 +486,7 @@ def main():
 
 
 if __name__ == "__main__":
-    """    
+    """
     build_wallet hot_wallet sEdVKkB9QaU7AZGYimWRiUsgZj6SrzM 28045792
     build_wallet cold_wallet sEd7R6eYgTxZ9zbPSUkG8eVLjSFnujQ 28045807
     build_wallet startup_wallet sEdSRcN7VznANrwooNxqneNu5oyarnr 28046821
