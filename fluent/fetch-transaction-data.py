f42e038a7837bd506b9ee367763636a5ce64ffee
injaelee <injaelee@gmail.com>
2022-07-08 12:23:15 -0700

    modified transaction fetcher to include payment paths

--- fluent/fetch-transaction-data.py
+++ fluent/fetch-transaction-data.py
@@ -8,6 +8,7 @@ import threading
 import time
 import unittest
 import logging
+import random
 
 logger = logging.getLogger(__name__)
 
@@ -54,11 +55,25 @@ class PaymentFetchProcessor(FetchProcessor):
     ):
         self.data_collector = data_collector
 
+    def _parse_paths(self,
+        paths: List[List[Dict[str,str]]],
+    ) -> List[List[str]]:
+        path_list = []
+        for path in paths:
+            resolved_path = []
+            for step in path:
+                currency = step.get("currency")
+                issuer = step.get("issuer")
+
+                cur = currency + ":" + issuer if issuer else currency
+                resolved_path.append(cur)
+            path_list.append(resolved_path)
+        return path_list
+
     def process(self,
         entry: Dict[str, Any],
         **kwargs,
     ):
-        
         if entry.get("TransactionType") != "Payment":
             return
 
@@ -66,20 +81,28 @@ class PaymentFetchProcessor(FetchProcessor):
         path_size = 0
 
         step_sizes = []
-        for path in entry.get("Paths", []):
-            path_size += 1
-            step_sizes.append(str(len(path)))
+        path_list = self._parse_paths(entry.get("Paths", []))
+        path_size = len(path_list)
+        paths_str = ""
+        for steps in path_list:
+            if paths_str:
+                paths_str += "-"
+            step_sizes.append(str(len(steps)))
+            paths_str += ">".join(steps)
 
         for affected_node in entry.get("metaData", {}).get("AffectedNodes", []):
             if affected_node.get("ModifiedNode", {}).get("LedgerEntryType") == "Offer":
                 offer_count += 1
+
+        # obtain the transaction status
+        txn_result = entry.get("metaData", {}).get("TransactionResult")
 
         ledger_index = kwargs.get("ledger_index")
         txn_hash = entry.get("hash")
 
         step_sizes_str = "|" + "\t".join(step_sizes) if len(step_sizes) > 0 else ""
         self.data_collector.collect(
-            f"{ledger_index}\t{txn_hash}\t{path_size}\t{offer_count}{step_sizes_str}")
+            f"{ledger_index}\t{txn_hash}\t{txn_result}\t{path_size}\t{offer_count}{step_sizes_str}\t*{paths_str}")
 
 
 class OutputQueueProcessor:
@@ -131,65 +154,18 @@ class XRPLedgerFetcherRPC:
         next_ledger_index_itr: ShardedLedgerIndexIterator,
         processors: List[FetchProcessor],
     ):
+        time.sleep(random.randrange(0,60)) # stagger start
         logger.info("[XRPLedgerFetcherRPC] Start fetching.")
 
         client = JsonRpcClient(self.url)
 
         for current_ledger_index in next_ledger_index_itr:
-            retry = 5    
-            while retry > 0:
-                itr_num = 0
-            
-                req = Ledger(
-                    ledger_index = current_ledger_index,
-                    transactions = True,
-                    expand = True,
-                )
 
-                itr_num += 1
-                response = client.request(req)
-                if not response.is_successful():
-                    retry -= 1
-                    logger.error(f"[{itr_num}] Received message has failure. Sleeping.")
-                    time.sleep(10)
-                    continue
-
-                message = response.result
-
-                txns = message.get("ledger").get("transactions")
-                ledger_index = message.get("ledger_index")
-
-                for txn in txns:
-                    for proc in processors:
-                        proc.process(
-                            txn,
-                            ledger_index = ledger_index,
-                        )
-
-                # break from the retry loop
-                break
-
-        logger.info("[XRPLedgerFetcherRPC] Finished exection.")
-
-
-class XRPLedgerFetcher:
-    def __init__(self,
-        url: str,
-    ):
-        self.url = url
-
-    def start_fetch(self,
-        next_ledger_index_itr: ShardedLedgerIndexIterator,
-        processors: List[FetchProcessor],
-    ):
-        logger.info("[XRPLedgerFetcher] Start fetching.")
-        with WebsocketClient(self.url) as client:
-            
-            for current_ledger_index in next_ledger_index_itr:
-                retry = 5    
-                while retry > 0:
+            retry = 0
+            while retry < 5:
+                try:
                     itr_num = 0
-                
+
                     req = Ledger(
                         ledger_index = current_ledger_index,
                         transactions = True,
@@ -199,9 +175,7 @@ class XRPLedgerFetcher:
                     itr_num += 1
                     response = client.request(req)
                     if not response.is_successful():
-                        retry -= 1
-                        logger.error(f"[{itr_num}] Received message has failure. Sleeping.")
-                        time.sleep(10)
+                        raise RuntimeError("Response was not successful.")
                         continue
 
                     message = response.result
@@ -217,9 +191,18 @@ class XRPLedgerFetcher:
                             )
 
                     # break from the retry loop
-                    break
+                except Exception as e:
+                    retry += 1
+                    sleep_time_s = 10 * (1.5) ** retry + random.randrange(2,8)
+                    logger.error(f"[{itr_num}] Received message has failure: {e}")
+                    logger.warn(f"Now sleeping for {sleep_time_s} seconds.")
+                    time.sleep(sleep_time_s)
+                    continue
 
-        logger.info("[XRPLedgerFetcher] Finished exection.")
+                # break retry loop when successful
+                break
+
+        logger.info("[XRPLedgerFetcherRPC] Finished exection.")
 
 
 class XRPLStatisticCollector:
@@ -259,7 +242,7 @@ class TestIterator(unittest.TestCase):
 
 def start_ledger_sequence() -> int:
     client = JsonRpcClient("https://s2.ripple.com:51234/")
-    return get_latest_validated_ledger_sequence(client) - 1
+    return 62715437 # get_latest_validated_ledger_sequence(client) - 1
 
 
 def single_threaded_start():
